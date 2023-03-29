2fbb4cd5b218eac5c1a39a350672f9a2d9b2b43e
franc <franc@DESKTOP-7D2EO6I>
2023-03-29 11:10:33 -0400

    Revert "Terceiro commit"
    This reverts commit bde26fc02367ea78605e3e0c635f701b1874c751.

--- PrimeiraClasse/src/TesteGithub.java
+++ PrimeiraClasse/src/TesteGithub.java
@@ -4,9 +4,6 @@ public class TesteGithub {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Segundo commit!");
-		
-		// terceiro commit
-		System.out.println("Terceiro commit!");
 	}
 
 }
