c0e8653cef717b3a7c9fe954780319781aa697ac
franc <franc@DESKTOP-7D2EO6I>
2023-03-29 18:43:21 -0400

    Testando clone do repositorio

--- PrimeiraClasse/src/TesteGithub.java
+++ PrimeiraClasse/src/TesteGithub.java
@@ -4,6 +4,9 @@ public class TesteGithub {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Segundo commit!");
+		
+		// clonando repositório
+		System.out.println("Segundo commit!");
 	}
 
 }
